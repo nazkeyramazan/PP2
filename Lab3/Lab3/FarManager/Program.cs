39bba38ca0562cce42868c794f6c5b26bbb59698
Ramazan Nazkey <nazkeyramazan@gmail.com>
2019-02-15 05:55:32 +0600

    maybe last commit

--- Lab3/Lab3/FarManager/Program.cs
+++ Lab3/Lab3/FarManager/Program.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 using System.IO;
 namespace FarManager
 {
+
     class Layer
     {
-        public int SelectedItem;
-        public FileSystemInfo[] items;
-
-        public Layer(FileSystemInfo[] items)
+        public int index;
+        public FileSystemInfo[] content;
+        public Layer(FileSystemInfo[] content)
         {
-            SelectedItem = 0;
-            this.items = items;
+            index = 0;
+            this.content = content;
         }
         public void Color(bool mode)
         {
@@ -22,17 +22,17 @@ namespace FarManager
             {
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.Clear();
-                for (int i = 0; i < items.Length; ++i)
+                for (int i = 0; i < content.Length; i++)
                 {
-                    if (i == SelectedItem)
+                    if (i == index)
                     {
-                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
                     }
                     else
                     {
                         Console.BackgroundColor = ConsoleColor.Black;
                     }
-                    if (items[i].GetType() == typeof(DirectoryInfo))
+                    if (content[i].GetType() == typeof(DirectoryInfo))
                     {
                         Console.ForegroundColor = ConsoleColor.Yellow;
                     }
@@ -40,95 +40,112 @@ namespace FarManager
                     {
                         Console.ForegroundColor = ConsoleColor.White;
                     }
-                    Console.WriteLine(items[i].Name);
+                    Console.WriteLine(content[i].Name);
                 }
             }
             else
             {
             }
         }
-    class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
-            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\LENOVO\Desktop\1half");//указывает путь к папке
-            Stack<Layer> history = new Stack<Layer>();
-                history.Push(new Layer(dirInfo.GetFileSystemInfos()));
-
-                bool quit = false;
-                bool mode = true; //1 - folder 0 - file
-
-            while (!quit)
+            static void Main(string[] args)
             {
+                DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\LENOVO\Desktop\folder");
+                Stack<Layer> history = new Stack<Layer>();
+                history.Push(new Layer(directoryInfo.GetFileSystemInfos()));
+                bool escape = false;
+                bool mode = true;
+                while (!escape)
+                {
                     history.Peek().Color(mode);
-                ConsoleKeyInfo pressedKey = Console.ReadKey();
-
-                    switch (pressedKey.Key)
+                    ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                    switch (consoleKeyInfo.Key)
                     {
-                        case ConsoleKey.UpArrow:
-                           if ( history.Peek().SelectedItem - 1 < 0)
-                            {
-                                history.Peek().SelectedItem = history.Peek().items.Length-1;
-                            }
-                            else 
-                            {
-                                history.Peek().SelectedItem--;
-                            }
-                            break;
-                        case ConsoleKey.DownArrow:
-                            if (history.Peek().SelectedItem + 1 >= history.Peek().items.Length )
-                            {
-                                history.Peek().SelectedItem = 0;
-                            }
-                            else
-                            {
-                                history.Peek().SelectedItem++;
-                            }
-                            break;
-                        case ConsoleKey.Enter:
-                            int x = history.Peek().SelectedItem;
-                            FileSystemInfo SelectedFSI = history.Peek().items[x];
-                            if(SelectedFSI.GetType() == typeof(DirectoryInfo))
-                            {
-                                FileSystemInfo[] items = (SelectedFSI as DirectoryInfo).GetFileSystemInfos();
-                                history.Push(new Layer(items));
-                            }
-                            else
-                            {
-                                mode = false;
-                                FileStream fs = new FileStream(SelectedFSI.FullName, FileMode.Open, FileAccess.Read);
-                                StreamReader sr = new StreamReader(fs);
-
-                                Console.BackgroundColor = ConsoleColor.White;
-                                Console.ForegroundColor = ConsoleColor.Black;
-                                Console.Clear();
-                                Console.WriteLine(sr.ReadToEnd());
-                                sr.Close();
-                                fs.Close();
-                            }
-                            break;
                         case ConsoleKey.Backspace:
                             mode = true;
                             history.Pop();
-                            /*
-                              if(mode == true)
-                            history.Pop();
+                            break;
+                        case ConsoleKey.Enter:
+                            int x = history.Peek().index;
+                            FileSystemInfo fileSystemInfo = history.Peek().content[x];
+                            if (fileSystemInfo.GetType() == typeof(DirectoryInfo) )
+                            {
+                                FileSystemInfo[] content = (fileSystemInfo as DirectoryInfo).GetFileSystemInfos();
+                                history.Push(new Layer(content));
+                            }
                             else
                             {
-                                Console.BackgroundColor = ConsoleColor.Black;
-                                Console.Clear();
-                                Console.ForegroundColor = ConsoleColor.White;
                                 mode = false;
+                                FileStream fs = new FileStream(fileSystemInfo.FullName, FileMode.Open, FileAccess.Read);
+                                StreamReader sr = new StreamReader(fs);
+                                Console.BackgroundColor = ConsoleColor.Blue;
+                                Console.Clear();
+                                Console.WriteLine(sr.ReadToEnd());
+                                fs.Close();
+                                sr.Close();
                             }
-                            */
+                            break;
+                        case ConsoleKey.UpArrow:
+                            if (history.Peek().index - 1 < 0)
+                            {
+                                history.Peek().index = history.Peek().content.Length - 1;
+                            }
+                            else
+                                history.Peek().index--;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (history.Peek().index + 1 >= history.Peek().content.Length)
+                            {
+                                history.Peek().index = 0;
+                            }
+                            else
+                                history.Peek().index++;
+                            break;
+                        case ConsoleKey.Delete:
+                            int x2 = history.Peek().index;
+                            FileSystemInfo fileSystemInfo2 = history.Peek().content[x2] ;
+                                history.Peek().index--;
+                            if( fileSystemInfo2.GetType() == typeof(DirectoryInfo))
+                            {
+                                DirectoryInfo dirInfo = fileSystemInfo2 as DirectoryInfo;
+                                Directory.Delete(fileSystemInfo2.FullName);
+                                history.Peek().content = dirInfo.Parent.GetFileSystemInfos();
+                            }
+                            else
+                            {
+                                FileInfo fileInfo = fileSystemInfo2 as FileInfo;
+                                File.Delete(fileSystemInfo2.FullName);
+                                history.Peek().content = fileInfo.Directory.GetFileSystemInfos();
+                            }
+                            break;
+                        case ConsoleKey.F2:
+                            
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+                            string name = Console.ReadLine();
+                            int x3 = history.Peek().index;
+                            FileSystemInfo fileSystemInfo3 = history.Peek().content[x3];
+                            if (fileSystemInfo3.GetType() == typeof(DirectoryInfo))
+                            {
+                                DirectoryInfo dirInfo = fileSystemInfo3 as DirectoryInfo;
+                                Directory.Move(fileSystemInfo3.FullName, dirInfo.Parent + "/" + name);
+                                history.Peek().content = dirInfo.Parent.GetFileSystemInfos();
+                            }
+                            else
+                            {
+                                FileInfo fileInfo = fileSystemInfo3 as FileInfo;
+                                File.Move(fileSystemInfo3.FullName, fileInfo.Directory + "/" + name);
+                                history.Peek().content = fileInfo.Directory.GetFileSystemInfos();
+                            }
                             break;
                         case ConsoleKey.Escape:
-                            quit = true;
+                            escape = true;
                             break;
-
-                    }      
-               }
+                    }
+                }
             }
         }
     }
 }
+
---
+++ Lab3/Lab3/part1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace part1
+{
+
+ class Layer
+ {
+    public int index;
+    public FileSystemInfo[] content;
+    public Layer(FileSystemInfo[] content)
+    {
+        index = 0;
+        this.content = content;
+    }
+    public void Color(bool mode)
+    {
+        if (mode == true)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (i == index)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                if (content[i].GetType() == typeof(DirectoryInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine(content[i].Name);
+            }
+        }
+        else
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\LENOVO\Desktop\1half");
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(new Layer(directoryInfo.GetFileSystemInfos()));
+            bool escape = false;
+            bool mode = true;
+            while (!escape)
+            {
+                history.Peek().Color(mode);
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.Backspace:
+                        mode = true;
+                        history.Pop();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (history.Peek().index - 1 < 0)
+                        {
+                            history.Peek().index = history.Peek().content.Length - 1;
+                        }
+                        else
+                            history.Peek().index--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (history.Peek().index + 1 >= history.Peek().content.Length)
+                        {
+                            history.Peek().index = 0;
+                        }
+                        else
+                            history.Peek().index++;
+                        break;
+                    case ConsoleKey.Escape:
+                        escape = true;
+                        break;
+                }
+            }
+        }
+    }
+}
+}
